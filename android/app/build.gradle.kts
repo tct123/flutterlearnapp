a5ea47e610c2dbe36940e693049bda1dda57c893
tct123 <42028373+tct123@users.noreply.github.com>
2025-05-11 08:14:49 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myweaterapp"
+    namespace = "de.cmg.myweaterapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
